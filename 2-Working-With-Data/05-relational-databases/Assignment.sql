4587e076210257462a4c4df392b0a305e55340e1
Onyeali Emmanuel <onyealistegen54@gmail.com>
2024-11-26 16:43:15 +0100

    update README with new quiz app link; add SQL queries for city and airport data

---
+++ 2-Working-With-Data/05-relational-databases/Assignment.sql
@@ -0,0 +1,14 @@
+-- SQLite
+
+-- Created queries to return the following information:
+
+-- Return all city names in the Cities table
+-- SELECT city FROM Cities;
+
+-- all cities in Ireland in the Cities table
+-- SELECT * FROM Cities WHERE country = 'Ireland';
+
+-- all airport names with their city and country using JOIN
+-- SELECT a.name, c.city, c.country FROM Airports a JOIN Cities c ON a.city_id = c.id;
+
+-- SELECT a.name FROM Airports a JOIN Cities c ON a.city_id = c.id WHERE c.city = 'London' AND c.country = 'United Kingdom';
